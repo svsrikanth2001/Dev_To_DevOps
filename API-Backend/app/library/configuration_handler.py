9b9ccecbb6cda31b1d981200fec2cabc41ca675d
svsrikanth2001 <emailsv@gmail.com>
2022-05-12 22:43:33 +0530

    commit

--- API-Backend/app/library/configuration_handler.py
+++ API-Backend/app/library/configuration_handler.py
@@ -2,7 +2,7 @@
 class ConfigurationHandler:
     def __init__(self):
         # define variables that you want to fetch and set from configuration service. Only use A-Z and _ in names.
-        self.DATABASE_CONNECTION_STRING = 'postgresql://postgres:password123!@64.227.179.202:5432/freshersonly'
+        self.DATABASE_CONNECTION_STRING = 'postgresql://postgres:password@localhost:5432/freshersonly'
         self.AUTH_JWT_SECRET = 'ABCD'
         self.AUTH_JWT_ALGORITHM = 'HS256'
 
