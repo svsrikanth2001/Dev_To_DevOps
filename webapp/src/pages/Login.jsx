9b8ea7c751637b21c11dc97b296cea303ea73557
Srikanth Venkata <srikanth.venkata@wefivesoft.com>
2022-12-30 14:29:41 +0530

    Login validation implementation

--- webapp/src/pages/Login.jsx
+++ webapp/src/pages/Login.jsx
@@ -3,8 +3,11 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
+import axios from "axios";
+const baseurl = 'http://localhost:8080/api-gateway/web';
 
 const Login = () => {
+  
   const {
     username,
     handleUserName,
@@ -17,14 +20,30 @@ const Login = () => {
   const {
     register,
     handleSubmit,
-    watch,
+    watch,setError,
     formState: { errors },
   } = useForm();
   const navigate = useNavigate();
   const onSubmit = (data) => {
-    handleUserName(data["username"]);
-    handleUserLoggedIn(true);
-    navigate("/dashboard");
+    //console.log('into on submit button');
+    //console.log(data);
+    axios.post(`${baseurl}/authenticate`, data
+    )
+    .then((response) => {
+      console.log(response);
+      if( response.status == 200 && response.statusText == 'OK') 
+      {
+        handleUserName(data["username"]);
+        handleUserLoggedIn(true);
+        navigate("/dashboard");
+      }
+    })
+    .catch((error) => {
+      
+      console.log(error);
+      setError("password", { type:"manual",message:"Password is incorrect!" }, { shouldFocus: true });
+    });
+
   };
   return (
     <div className="relative flex flex-col justify-center overflow-hidden min-h-screen">
@@ -37,8 +56,9 @@ const Login = () => {
           </label>
           <input
            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            {...register("username")}
+            {...register("username", { required: true })}
           />
+            {errors.username && <p>This is a required field!</p>}
         </div>
         <div className="mb-2">
           <label htmlFor="password"  className="block text-sm font-semibold text-gray-800">
@@ -49,6 +69,7 @@ const Login = () => {
             {...register("password", { required: true })}
             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
           />
+        {errors.password && <p>{errors.password.message}</p>}
         </div>
         <div>
           <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
